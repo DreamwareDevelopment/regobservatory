11b2a3c12b1cca4da7401473e0914c81768f83e8
Dreamware Development <zander@dreamwaredevelopment.com>
2025-02-13 22:42:22 -0700

    Update site meta

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import AgencySearch from "./components/search";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-between min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div id="root" className="flex flex-col items-center justify-between min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="container mx-auto p-4 flex flex-col gap-4 items-center">
         <h1 className="text-2xl font-bold mb-4">RegObservatory</h1>
         <AgencyHistoryChart />
